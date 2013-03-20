f3bb2bfed79058154c6e22765701e402453bc930
ramntry <ramntry@gmail.com>
2013-03-20 16:40:30 +0400

    yet another small optimisation for task 1 of exam 4

--- ex1/task1/optimized_version_made_at_home_after_the_exam.hs
+++ ex1/task1/optimized_version_made_at_home_after_the_exam.hs
@@ -2,12 +2,12 @@ module Main where
 
 -- Compile with -O2, please
 
-isPrime :: (Integral a) => [a] -> a -> Bool
+isPrime :: [Int] -> Int -> Bool
 isPrime deviders n = foldr step True deviders
     where step d acc | fromIntegral d > (sqrt . fromIntegral) n = True
                      | otherwise = (n `mod` d /= 0) && acc
 
-primes :: (Integral a) => [a]
+primes :: [Int]
 primes = 2 : filter (isPrime primes) [3..]
 
 main = putStrLn ("100'000th prime number is "
